c0d180e76e9276c86a5ea1d10c746a93de847a71
LukasHechenberger <LukasHechenberger@users.noreply.github.com>
2020-07-24 15:27:09 +0200

    style: Remove unused code

--- packages/atvise-scripts/src/scripts/prepare.ts
+++ packages/atvise-scripts/src/scripts/prepare.ts
@@ -1,20 +1,2 @@
-import { promises as fsp } from 'fs';
-import writePkg from 'write-pkg';
-import { load } from '../lib/config';
-import type { ScriptRunnerOptions } from '.';
-
 export const name = 'prepare';
 export const description = 'Prepare project for atvise-scripts';
-
-export async function run({ info }: ScriptRunnerOptions) {
-  const pkg = JSON.parse(await fsp.readFile('./package.json', 'utf8'));
-  const config = await load({ fallbackToDefaults: true });
-
-  const proxyUrl = `http://${config.host}:${config.port.http}`;
-
-  if (pkg.proxy !== proxyUrl) {
-    info('Updating proxy settings...');
-    pkg.proxy = proxyUrl;
-    writePkg('./package.json', pkg);
-  }
-}
