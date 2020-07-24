5ac18da334e9b1e22700b861504de2c19c3fcbcb
Lukas Hechenberger <LukasHechenberger@users.noreply.github.com>
2020-07-24 15:40:34 +0200

    perf(atvise-scripts): Lazy-load script actions (#4)

--- packages/atvise-scripts/src/scripts/prepare.ts
+++ packages/atvise-scripts/src/scripts/prepare.ts
@@ -1,23 +1,2 @@
-import { promises as fsp } from 'fs';
-import writePkg from 'write-pkg';
-import { load } from '../lib/config';
-import type { Script } from '.';
-
-const prepare: Script = {
-  name: 'prepare',
-  description: 'Prepare project for atvise-scripts',
-  async run({ info }) {
-    const pkg = JSON.parse(await fsp.readFile('./package.json', 'utf8'));
-    const config = await load({ fallbackToDefaults: true });
-
-    const proxyUrl = `http://${config.host}:${config.port.http}`;
-
-    if (pkg.proxy !== proxyUrl) {
-      info('Updating proxy settings...');
-      pkg.proxy = proxyUrl;
-      writePkg('./package.json', pkg);
-    }
-  },
-};
-
-export default prepare;
+export const name = 'prepare';
+export const description = 'Prepare project for atvise-scripts';
---
+++ packages/atvise-scripts/src/scripts/run/prepare.ts
@@ -0,0 +1,17 @@
+import { promises as fsp } from 'fs';
+import writePkg from 'write-pkg';
+import { load } from '../../lib/config';
+import type { ScriptRunnerOptions } from '..';
+
+export default async function runPrepare({ info }: ScriptRunnerOptions) {
+  const pkg = JSON.parse(await fsp.readFile('./package.json', 'utf8'));
+  const config = await load({ fallbackToDefaults: true });
+
+  const proxyUrl = `http://${config.host}:${config.port.http}`;
+
+  if (pkg.proxy !== proxyUrl) {
+    info('Updating proxy settings...');
+    pkg.proxy = proxyUrl;
+    writePkg('./package.json', pkg);
+  }
+}
