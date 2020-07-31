3c091a421271790c70350494310b6408e8792251
LukasHechenberger <LukasHechenberger@users.noreply.github.com>
2020-07-31 09:25:53 +0200

    chore(modular-webmi): Build declaration files

--- packages/modular-webmi/rollup.config.js
+++ packages/modular-webmi/rollup.config.js
@@ -1,11 +1,10 @@
 import { builtinModules } from 'module';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import { dependencies } from './package.json';
 
 export default {
   input: ['./src/index.ts'],
-  external: [...builtinModules, ...Object.keys(dependencies)],
+  external: [...builtinModules],
   plugins: [resolve(), typescript()],
   output: [
     {
