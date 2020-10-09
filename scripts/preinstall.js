124e55c4e7db43213a0b163e8759313b98fa1c38
LukasHechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2020-10-09 11:12:21 +0200

    style: Fix typo

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -4,7 +4,7 @@ const { promises: fsp } = require('fs');
 const { dirname } = require('path');
 
 async function preinstall() {
-  const file = 'packages/atvise-scripts/node_modules/atvise-scripts/out/bin.js';
+  const file = 'packages/atvise-scripts/out/bin.js';
 
   await fsp.mkdir(dirname(file), { recursive: true });
   await fsp.stat(file).catch(async (error) => {
